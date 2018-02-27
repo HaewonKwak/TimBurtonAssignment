197406184f229c3ff7dc6c2fbb3b6f1701016dad
Haewon Kwak <Haewon.Kwak@pointclickcare.com>
2018-02-26 20:51:40 -0500

    Initial Commit

---
+++ Tim Burtons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tim Burtons
+//
+//  Created by Haewon Kwak on 2018-02-26.
+//  Copyright © 2018 Tim Burtons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
